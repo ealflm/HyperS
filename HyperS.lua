248cd98734d0f8c0daba9e1894efabe3c5ec9c17
ealflm <ealflm@gmail.com>
2023-11-13 19:05:51 +0700

    commit

--- HyperS.lua
+++ HyperS.lua
@@ -5,6 +5,7 @@ ElapsedTime = 0
 
 function Initialize()
 	MeasureTime = SKIN:GetMeasure(SELF:GetOption('TimeMeasure', 'MeasureTime'))
+	DeltaFilePath = SKIN:GetMeasure(SELF:GetOption('DeltaFilePath'))
 end
 
 function Update()
@@ -22,10 +23,42 @@ function FormatTimeString(time)
 	return hours .. ':' .. minutes .. ':' .. seconds
 end
 
+function GetDelta()
+    local filePath = os.getenv("NotionTimeTrackerDelta")
+    SKIN:Bang("!Log", filePath)
+
+    if filePath then
+        local file = io.open(filePath, "r")
+
+        if file then
+            local content = file:read("*a")
+
+            file:close()
+
+            return Iso8601ToWindowsTimestamp_7(content)
+        else
+            SKIN:Bang("!Log", "Can open file")
+            return 0
+        end
+    else
+        SKIN:Bang("!Log", "Can open file")
+        return 0
+    end
+end
+
+function Iso8601ToWindowsTimestamp_7(iso8601)
+  local year, month, day, hour, min, sec = iso8601:match("(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)Z")
+  local timestamp = os.time{year=year, month=month, day=day, hour=hour, min=min, sec=sec}
+  return timestamp + 11644473600 + 25200 * 2
+end
+
 function Start()
-  DeltaTime = MeasureTime:GetValue()
-  SKIN:Bang("!EnableMeasure", "MeasureStopwatchScript")
-  SKIN:Bang("!UpdateMeter", "MeterStopwatchMainDisplay")
+  SKIN:Bang("!Log", MeasureTime:GetValue())
+  DeltaTime = GetDelta()
+  if (DeltaTime ~= 0) then
+    SKIN:Bang("!EnableMeasure", "MeasureStopwatchScript")
+    SKIN:Bang("!UpdateMeter", "MeterStopwatchMainDisplay")
+  end
 end
 
 function Reset()
